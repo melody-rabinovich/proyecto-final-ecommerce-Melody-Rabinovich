d84c2f1cf16dba36553a7345c2756084e5e15ff9
melody-rabinovich <melodyrabinovich@gmail.com>
2025-07-16 19:57:23 -0300

    arreglo firebase

--- src/models/firebase.js
+++ src/models/firebase.js
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
+import dotenv from 'dotenv';
+dotenv.config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB4trtJQjOLXoeqI0XW765vDSgi0e5um-w",
-  authDomain: "api-talento-tech-7bc8f.firebaseapp.com",
-  projectId: "api-talento-tech-7bc8f",
-  storageBucket: "api-talento-tech-7bc8f.firebasestorage.app",
-  messagingSenderId: "105241477830",
-  appId: "1:105241477830:web:1b5dbd37ebec7dc1634dc7",
-  measurementId: "G-CHMHLNV8WN"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
